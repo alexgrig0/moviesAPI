80c1e7a89ae712af1a9a674a24bee786e9228de6
alexgrig0 <alexandru.grigore444@gmail.com>
2023-03-12 04:39:48 +0200

    first commit - backend only

---
+++ src/main/java/com/moviesproject/movies/models/Review.java
@@ -0,0 +1,24 @@
+package com.moviesproject.movies.models;
+
+import lombok.*;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document
+@Data
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    @Id
+    private ObjectId id;
+    private String body;
+
+    public Review(String body) {
+        this.body = body;
+    }
+}
